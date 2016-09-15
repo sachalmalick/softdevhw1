1e94f0046fccca91fc42ed61c4d619ba40cd2a65
Kathy Lau <slau7@stuy.edu>
2016-09-15 14:02:04 -0400

    Update occupation.py

--- occupation.py
+++ occupation.py
@@ -3,8 +3,7 @@ import random
 f = open('occupations.csv', 'r')
 lfile = f.readlines() #list of occupations
 
-d = {}
-i = 0
+
 tmp = 0
 L=[]
 for row in lfile[1:-1]:
@@ -13,15 +12,11 @@ for row in lfile[1:-1]:
     tmp = num + start
     #print "start is" + str(start) + "end is" + str(start + num)
     L.append([start, tmp, row[:row.rfind(',')]])
-    #[row[:row.rfind(',')]] = [start, tmp] 
-    #tmp +=num
-    i+=1
 
 #print L
 
 def chooser():
     rand = int(random.random() * 1000)
-    chosen = False
     for row in L:
         if rand > row[0] and rand < row[1]:
             return row[2]
